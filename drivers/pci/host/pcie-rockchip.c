c4c5865bacc799076d8bd45bf0cd6daeafaa369d
Caesar Wang <wxt@rock-chips.com>
2019-09-29 03:28:58 +0000

    Revert "PCI: rockchip: limit PCIe accessors within 1MB cfg space"
    This reverts commit 6c49045c2980dc52a7ad4eb7fbf765c2e5e5d48a.
    
    That will cause the pci-e/nvme interface device can't work properly.
    
    Change-Id: Ibc0a48e961e22262bbe493bc1dc78ee666baba43
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- drivers/pci/host/pcie-rockchip.c
+++ drivers/pci/host/pcie-rockchip.c
@@ -459,6 +459,7 @@ static int rockchip_pcie_wr_other_conf(struct rockchip_pcie *rockchip,
 
 	if (bus->number > 0x1f)
 		return PCIBIOS_DEVICE_NOT_FOUND;
+	
 	if (!IS_ALIGNED(busdev, size))
 		return PCIBIOS_BAD_REGISTER_NUMBER;
 
