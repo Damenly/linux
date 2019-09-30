687874da84a3634ab1a5186cc6223fe4fea0d00c
Simon Xue <xxm@rock-chips.com>
2019-09-30 10:26:19 +0800

    PCI: rockchip: dw: support turn off phy pd when suspend
    1. keep atu index when resume
    2. reconfigure phy and atu when resume
    
    Change-Id: Iaf0a9f9a10e2f6c25a933cc808e1d7fcd78103b5
    Signed-off-by: Simon Xue <xxm@rock-chips.com>

--- drivers/pci/host/pcie-dw-rockchip.c
+++ drivers/pci/host/pcie-dw-rockchip.c
@@ -135,6 +135,7 @@ struct rk_pcie {
 	struct regmap			*usb_pcie_grf;
 	struct regmap			*pmu_grf;
 	struct dma_trx_obj		*dma_obj;
+	bool in_suspend;
 };
 
 struct rk_pcie_of_data {
@@ -399,11 +400,15 @@ static int rk_pcie_ep_inbound_atu(struct rk_pcie *rk_pcie,
 	int ret;
 	u32 free_win;
 
-	free_win = find_first_zero_bit(rk_pcie->ib_window_map,
-				       rk_pcie->num_ib_windows);
-	if (free_win >= rk_pcie->num_ib_windows) {
-		dev_err(rk_pcie->dev, "No free inbound window\n");
-		return -EINVAL;
+	if (rk_pcie->in_suspend) {
+		free_win = rk_pcie->bar_to_atu[bar];
+	} else {
+		free_win = find_first_zero_bit(rk_pcie->ib_window_map,
+					       rk_pcie->num_ib_windows);
+		if (free_win >= rk_pcie->num_ib_windows) {
+			dev_err(rk_pcie->dev, "No free inbound window\n");
+			return -EINVAL;
+		}
 	}
 
 	ret = rk_pcie_prog_inbound_atu(rk_pcie, free_win, bar, cpu_addr,
@@ -412,6 +417,10 @@ static int rk_pcie_ep_inbound_atu(struct rk_pcie *rk_pcie,
 		dev_err(rk_pcie->dev, "Failed to program IB window\n");
 		return ret;
 	}
+
+	if (rk_pcie->in_suspend)
+		return 0;
+
 	rk_pcie->bar_to_atu[bar] = free_win;
 	set_bit(free_win, rk_pcie->ib_window_map);
 
@@ -424,15 +433,23 @@ static int rk_pcie_ep_outbound_atu(struct rk_pcie *rk_pcie,
 {
 	u32 free_win;
 
-	free_win = find_first_zero_bit(rk_pcie->ob_window_map,
-				       rk_pcie->num_ob_windows);
-	if (free_win >= rk_pcie->num_ob_windows) {
-		dev_err(rk_pcie->dev, "No free outbound window\n");
-		return -EINVAL;
+	if (rk_pcie->in_suspend) {
+		free_win = find_first_bit(rk_pcie->ob_window_map,
+					  rk_pcie->num_ob_windows);
+	} else {
+		free_win = find_first_zero_bit(rk_pcie->ob_window_map,
+					       rk_pcie->num_ob_windows);
+		if (free_win >= rk_pcie->num_ob_windows) {
+			dev_err(rk_pcie->dev, "No free outbound window\n");
+			return -EINVAL;
+		}
 	}
 
 	rk_pcie_prog_outbound_atu(rk_pcie, free_win, PCIE_ATU_TYPE_MEM,
 				  phys_addr, pci_addr, size);
+
+	if (rk_pcie->in_suspend)
+		return 0;
 
 	set_bit(free_win, rk_pcie->ob_window_map);
 	rk_pcie->outbound_addr[free_win] = phys_addr;
@@ -893,6 +910,8 @@ static int rk_pcie_phy_init(struct rk_pcie *rk_pcie)
 		return ret;
 	}
 
+	phy_power_on(rk_pcie->phy);
+
 	return 0;
 }
 
@@ -1184,6 +1203,8 @@ static int rk_pcie_probe(struct platform_device *pdev)
 
 	rk_pcie_dbi_ro_wr_dis(rk_pcie);
 
+	device_init_wakeup(dev, true);
+
 	return 0;
 
 deinit_clk:
@@ -1193,6 +1214,106 @@ deinit_clk:
 }
 
 MODULE_DEVICE_TABLE(of, rk_pcie_of_match);
+
+static inline void rk_pcie_link_status_clear(struct rk_pcie *rk_pcie)
+{
+	rk_pcie_writel_apb(rk_pcie, PCIE_CLIENT_GENERAL_DEBUG, 0x0);
+}
+
+static inline void rk_pcie_disable_ltssm(struct rk_pcie *rk_pcie)
+{
+	rk_pcie_writel_apb(rk_pcie, 0x0, 0xc0008);
+}
+
+static int __maybe_unused rockchip_dw_pcie_suspend(struct device *dev)
+{
+	struct rk_pcie *rk_pcie = dev_get_drvdata(dev);
+
+	rk_pcie_link_status_clear(rk_pcie);
+	rk_pcie_disable_ltssm(rk_pcie);
+
+	/* make sure assert phy success */
+	usleep_range(200, 300);
+
+	phy_power_off(rk_pcie->phy);
+	phy_exit(rk_pcie->phy);
+
+	clk_bulk_disable(rk_pcie->clk_cnt, rk_pcie->clks);
+
+	rk_pcie->in_suspend = true;
+
+	return 0;
+}
+
+static int __maybe_unused rockchip_dw_pcie_resume(struct device *dev)
+{
+	struct rk_pcie *rk_pcie = dev_get_drvdata(dev);
+	int ret;
+
+	ret = clk_bulk_enable(rk_pcie->clk_cnt, rk_pcie->clks);
+	if (ret) {
+		clk_bulk_unprepare(rk_pcie->clk_cnt, rk_pcie->clks);
+		return ret;
+	}
+
+	ret = phy_set_mode(rk_pcie->phy, rk_pcie->phy_mode);
+	if (ret) {
+		dev_err(dev, "fail to set phy to mode %s, err %d\n",
+			(rk_pcie->phy_mode == PHY_MODE_PCIE_RC) ? "RC" : "EP",
+			ret);
+		return ret;
+	}
+
+	ret = phy_init(rk_pcie->phy);
+	if (ret < 0) {
+		dev_err(dev, "fail to init phy, err %d\n", ret);
+		return ret;
+	}
+
+	phy_power_on(rk_pcie->phy);
+
+	rk_pcie_dbi_ro_wr_en(rk_pcie);
+
+	/* release link reset grant */
+	ret = rk_pcie_reset_grant_ctrl(rk_pcie, true);
+	if (ret)
+		return ret;
+
+	/* Set PCIe mode */
+	rk_pcie_set_mode(rk_pcie);
+	/* Set PCIe gen2 */
+	rk_pcie_set_gens(rk_pcie);
+
+	ret = rk_pcie_establish_link(rk_pcie);
+	if (ret) {
+		dev_err(dev, "failed to establish pcie link\n");
+		return ret;
+	}
+
+	ret = rk_pcie_ep_atu_init(rk_pcie);
+	if (ret) {
+		dev_err(dev, "failed to init ep device\n");
+		return ret;
+	}
+
+	rk_pcie_ep_setup(rk_pcie);
+
+	/* hold link reset grant after link-up */
+	ret = rk_pcie_reset_grant_ctrl(rk_pcie, false);
+	if (ret)
+		return ret;
+
+	rk_pcie_dbi_ro_wr_dis(rk_pcie);
+
+	rk_pcie->in_suspend = false;
+
+	return 0;
+}
+
+static const struct dev_pm_ops rockchip_dw_pcie_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(rockchip_dw_pcie_suspend,
+				rockchip_dw_pcie_resume)
+};
 
 static struct platform_driver rk_plat_pcie_driver = {
 	.driver = {
